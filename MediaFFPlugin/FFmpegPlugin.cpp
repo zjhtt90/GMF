fece8ffa25459286a428fab0e672c7a70a847e84
zjhtt90 <zjhcanghai@163.com>
2021-02-05 23:52:22 +0800

    change ffmpeg plugin

---
+++ MediaFFPlugin/FFmpegPlugin.cpp
@@ -0,0 +1,58 @@
+#include "FFCameraCapture.h"
+#include "FFVideoScaler.h"
+#include "FFH264Encoder.h"
+#include "FFAACEncoder.h"
+#include "FFVideoDecoder.h"
+#include "FFAudioResample.h"
+#include "FFAudioDecoder.h"
+#include "FFScreenCapture.h"
+#include "FFSimpleFilter.h"
+#include "FFComplexFilter.h"
+#include "FFRTSPServer.h"
+
+#include "Plugin.h"
+
+static int plugin_init(CPlugin *plugin)
+{
+	av_register_all();
+	avdevice_register_all();
+	avcodec_register_all();
+	avfilter_register_all();
+
+	//av_log_set_level(AV_LOG_TRACE);
+
+	plugin->AddElement("FFScreenCapture", new CFFScreenCaptureFactory());
+	plugin->AddElement("FFCameraCapture", new CFFCameraCaptureFactory());
+
+	plugin->AddElement("FFSimpleFilter", new CFFSimpleFilterFactory());
+	plugin->AddElement("FFComplexFilter", new CFFComplexFilterFactory());
+
+	plugin->AddElement("FFH264Encoder", new CFFH264EncoderFactory());
+	plugin->AddElement("FFAACEncoder", new CFFAACEncoderFactory());
+	plugin->AddElement("FFVideoScaler", new CFFVideoScalerFactory());
+
+	plugin->AddElement("FFAudioResample", new CFFAudioResampleFactory());
+	plugin->AddElement("FFVideoDecoder", new CFFVideoDecoderFactory());
+	plugin->AddElement("FFAudioDecoder", new CFFAudioDecoderFactory());
+
+	plugin->AddElement("FFRTSPServer", new CFFRTSPServerFactory());
+
+	return MEDIA_ERR_NONE;
+}
+
+static void plugin_uninit(CPlugin *plugin)
+{
+	plugin->RemoveElement("FFScreenCapture");
+	plugin->RemoveElement("FFCameraCapture");
+	plugin->RemoveElement("FFSimpleFilter");
+	plugin->RemoveElement("FFComplexFilter");
+	plugin->RemoveElement("FFH264Encoder");
+	plugin->RemoveElement("FFAACEncoder");
+	plugin->RemoveElement("FFVideoScaler");
+	plugin->RemoveElement("FFAudioResample");
+	plugin->RemoveElement("FFVideoDecoder");
+	plugin->RemoveElement("FFAudioDecoder");
+	plugin->RemoveElement("FFRTSPServer");
+}
+
+PLUGIN_DEFINE(ffmpeg, "Media FFmpeg plugin", plugin_init, plugin_uninit)
